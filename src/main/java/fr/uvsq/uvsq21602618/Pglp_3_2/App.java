1a80136a21d549eb07c59f9048125bef953ab816
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-02-26 11:06:46 +0100

    creation classe Employe

---
+++ src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21602618.Pglp_3_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/Employe.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21602618.Pglp_3_2;
+
+public class Employe {
+	private final String nom;
+	private final int date_arrivee;
+	
+	public Employe(String nom,int date) {
+		this.nom = nom;
+		this.date_arrivee=date;
+		
+	}
+
+}
