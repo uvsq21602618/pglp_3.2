96622e0a7bedcc2a52b090746dfcc72fd3153967
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-02-26 11:44:41 +0100

    creation classe Vendeur

---
+++ src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/Vendeur.java
@@ -0,0 +1,23 @@
+package fr.uvsq.uvsq21602618.Pglp_3_2;
+
+import java.util.Calendar;
+
+public class Vendeur extends Employe{
+
+	private double commission;
+	
+	public Vendeur(String nom, int annee) {
+		super(nom, annee);
+		this.commission=0;
+	}
+	
+	@Override
+	public double calculSalaire() {
+		return super.calculSalaire()+commission;
+	}
+	
+	public void setCommission(double commission) {
+		this.commission=commission;
+	}
+
+}
