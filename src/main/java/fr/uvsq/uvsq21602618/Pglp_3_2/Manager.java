855dac71dc217d59dc14a76f5009581908a034ad
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-02-26 11:58:59 +0100

    creation methodes setNbSubalternes et calculSalaire

--- src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/Manager.java
+++ src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/Manager.java
@@ -2,8 +2,20 @@ package fr.uvsq.uvsq21602618.Pglp_3_2;
 
 public class Manager extends Employe {
 
+	private int nb_subalternes;
+	
 	public Manager(String nom, int annee) {
 		super(nom, annee);
+		nb_subalternes=1;
+	}
+	
+	@Override
+	public double calculSalaire() {
+		return super.calculSalaire()+nb_subalternes*100;
+	}
+	
+	public void setNbSubalternes(int nb) {
+		this.nb_subalternes=nb;
 	}
 
 }
