e1f0028e4aaacf46ca72a8ec31853dc09910be9c
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-02-26 12:09:25 +0100

    creation classe Entreprise

---
+++ src/main/java/fr/uvsq/uvsq21602618/Pglp_3_2/Entreprise.java
@@ -0,0 +1,14 @@
+package fr.uvsq.uvsq21602618.Pglp_3_2;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Entreprise {
+	
+	private ArrayList<Employe> listeEmp;
+	
+	public Entreprise() {
+		listeEmp = new ArrayList<Employe>();
+	}
+	
+}
